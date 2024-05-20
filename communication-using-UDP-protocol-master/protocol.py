efbd1e58d68c50449721c0c388c161bbf02b00d6
Akshaj-Jauhri <86787922+Akshaj-Jauhri@users.noreply.github.com>
2024-05-20 20:52:02 +0530

    Add files via upload

---
+++ communication-using-UDP-protocol-master/protocol.py
@@ -0,0 +1,176 @@
+import enum
+import re
+
+HEADER_SIZE = 6
+DEFAULT_BUFF = 4096
+DEFAULT_FRAGMENT_LEN = 4
+FRAGMENT_MAX = 1466         # max_fragment = data(1500) - UDP header(8) - IP header(20) - my header(6) = 1466
+FRAGMENT_MIN = 1            # min_fragment = data(46) - UDP header(8) - IP header(20) - my header(6) = 12
+CRC_KEY = '1001'            # x^3 + 1
+
+
+class MsgType(enum.Enum):
+    """ Enum with constant as signal msg in protocol. """
+
+    SET = '0'       # constant for header in initialization for file transfer
+    PSH = '1'       # constant for header for pushing for file transfer
+    ACK = '2'       # constant for header for positive acknowledgment
+    RST = '3'       # constant for header for negative acknowledgment
+
+    SET_MSG = '8'   # constant for header in initialization for msg transfer
+
+
+class MsgReply(enum.Enum):
+    """ Enum with constant as output for client - server communication. """
+
+    SET = 'Initialization successful'   # msg initialization
+    ACK = 'Message received!'           # msg for positive acknowledgment
+    RST = 'Message corrupted'           # msg for negative acknowledgment
+    KAP = 'Connected'                   # msg for keep alive packet
+
+
+def zero_fill(data):
+    """ Fill fragment size with zeroes if less than default fragment length, which is set to 4.
+
+     data: Data to be filled with zeroes. """
+
+    if DEFAULT_FRAGMENT_LEN == len(data):
+        return data
+    count = DEFAULT_FRAGMENT_LEN - len(data) - 1
+    new_data = b'0'
+    while count > 0:
+        new_data += b'0'
+        count -= 1
+    new_data += data
+    return new_data
+
+
+def xor(a, b):
+    """ Logical XOR method for CRC. Returns string of bits.
+    a XOR b
+
+    a: Bit number for XOR.
+    b: Bit number for XOR. """
+
+    result = []                                            # initialize result
+    for i in range(1, len(b)):                             # go through all bits
+        if a[i] == b[i]:                                   # same - XOR is 0
+            result.append('0')
+            continue
+        result.append('1')                                 # not same - XOR is 1
+    return ''.join(result)
+
+
+def sum_checksum(checksum):
+    """ Count all bits from given checksum. Return string of this count.
+
+    checksum: Computed checksum to sum all bits. """
+
+    sum_digit = 0
+    for char in checksum:
+        if char.isdigit():
+            sum_digit += int(char)
+    return str(sum_digit)
+
+
+def set_crc(data):
+    """ Get checksum based on CRC method. Returns summary of all checksum bits as string.
+
+     data: Data for CRC. """
+
+    crc_key_len = len(CRC_KEY)
+    bin_data = (''.join(format(ord(char), 'b') for char in str(data))) + ('0' * (crc_key_len - 1))
+    checksum = bin_data[:crc_key_len]
+    while crc_key_len < len(bin_data):
+        if checksum[0] == '1':
+            checksum = xor(CRC_KEY, checksum) + bin_data[crc_key_len]
+        else:
+            checksum = xor('0' * crc_key_len, checksum) + bin_data[crc_key_len]
+        crc_key_len += 1
+    if checksum[0] == '1':
+        checksum = xor(CRC_KEY, checksum)
+    else:
+        checksum = xor('0' * crc_key_len, checksum)
+    return sum_checksum(checksum)
+
+
+def check_crc(data):
+    """ Check if received data was transferred correctly. Returns positive acknowledgment enum if received data is
+    correct, otherwise returns negative acknowledgment enum.
+
+    data: Data contains protocol header with information about original computed checksum. Computing checksum for
+    received data and comparing it with original checksum gives a return value. """
+
+    if data[5:6].decode('utf-8') != set_crc(data[6:]):
+        return MsgType.RST
+    return MsgType.ACK
+
+
+def get_fragmet_size(data):
+    """ Getter method for getting fragment size from protocol header. Returns fragment size as int.
+
+    data: Data contains protocol header with information about fragment size entered by user. """
+    return int(data[1:5].decode('utf-8'))
+
+
+def get_file_name(data, fragment_count):
+    """ Getter method for getting file name from protocol header. Returns file name as string.
+
+    data: Data contains received data from transfer initialization with information about file name entered by user. """
+    return data[6:-len(fragment_count)].decode('utf-8')
+
+
+def get_data(data):
+    """ Getter method for getting data without protocol header. Returns received data as bytes.
+
+    data: Data contains received data with protocol header. """
+    return data[6:]
+
+
+def get_fragment_count(data, msg_type=None):
+    """ Getter method for getting fragment count from data. Returns fragment count as string.
+    For text message transfer just returns data from index 7(6-bit header + 1-bit data).
+    For file transfer returns last number from data, because data contains also file name it is possible to have data as
+    name1.pdf720, where last number 720 is fragment count.
+
+    data: Data contains received data with protocol header. """
+
+    if msg_type == MsgType.SET_MSG.value:
+        return data.decode('utf-8')[7:]
+    return re.compile(r'\d+').findall(data.decode('utf-8'))[-1:][0]     # regex; getting last number from file name
+
+
+def get_msg_type(data):
+    """ Getter method for getting signal message type.
+
+    data: Data contains received data with protocol header. """
+    return data[:1]
+
+
+def add_header(msg_type, fragment_size, data):
+    """ Adding protocol header to data. Returns new data with protocol header as bytes.
+
+    msg_type: Type of message as enum MsgType.
+    fragment_size: Maximum size of received data fragment. Entered by a user.
+    data: Data contains received data without protocol header. """
+
+    fragment_size_bytes = zero_fill(bytes(str(fragment_size), 'utf-8'))
+    new_data = bytes(msg_type.value, 'utf-8') + fragment_size_bytes
+    checksum = set_crc(data)
+    new_data += bytes(checksum, 'utf-8') + data
+    return new_data
+
+
+def msg_initialization(fragment_size, data):
+    """ Adding protocol header to data for initialization. Returns new data with protocol header as bytes.
+
+    fragment_size: Maximum size of received data fragment. Entered by a user.
+    data: Data contains received data without protocol header. """
+
+    new_data = bytes(MsgType.SET.value, 'utf-8') if data[:1] != bytes(MsgType.SET_MSG.value, 'utf-8') else \
+        bytes(MsgType.SET_MSG.value, 'utf-8')                               # add msg type
+    fragment_size_bytes = zero_fill(bytes(str(fragment_size), 'utf-8'))     # fill fragment size with zeros, if less 4
+    checksum = set_crc(data)                                                # get checksum
+    new_data += fragment_size_bytes + bytes(checksum, 'utf-8')              # add fragment size and checksum
+    new_data += data                                                        # add data
+    return new_data                                                         # return header + data as new data
---
+++ communication-using-UDP-protocol-master/server.py
@@ -0,0 +1,161 @@
+import client
+import protocol
+import socket
+import sys
+import os
+import select
+
+def write_msg(server_socket, fragment_size, fragment_no):
+    """ Write received text message to a console.
+
+    server_socket: Server socket contains source address and sendto method.
+    fragment_size: Maximum size of received data fragment. Entered by a user.
+    fragment_no: Total count of fragments. """
+
+    fragment_count = 0
+    while True:
+        ready = select.select([server_socket], [], [], 3)
+        if ready[0]:
+            message, client_address = server_socket.recvfrom(protocol.DEFAULT_BUFF)
+            reply_crc = protocol.check_crc(message)
+            reply = protocol.add_header(reply_crc, fragment_size, b'')
+            server_socket.sendto(reply, client_address)
+            if reply_crc.value != protocol.MsgType.ACK.value:                       # ARQ Stop & Wait
+                continue
+            message = protocol.get_data(message)
+            print(message.decode('utf-8'), end='')
+            fragment_count += 1
+        else:
+            break
+    print('\nReceived fragments are', fragment_count, 'and counted fragments are', fragment_no, '\n')
+
+
+def write_file(path, server_socket, fragment_size, fragment_no):
+    """ Write received file to a server system.
+
+    server_socket: Server socket contains source address and sendto method.
+    path: Information where to store received file.
+    fragment_size: Maximum size of received data fragment. Entered by a user.
+    fragment_no: Total count of fragments. """
+
+    fragment_count = 0
+    with open(path, 'wb+') as file:
+        while True:
+            ready = select.select([server_socket], [], [], 3)
+            if ready[0]:
+                data, client_address = server_socket.recvfrom(fragment_size)
+                reply_crc = protocol.check_crc(data)
+                reply = protocol.add_header(reply_crc, fragment_size, b'')
+                server_socket.sendto(reply, client_address)
+
+                if reply_crc.value != protocol.MsgType.ACK.value:
+                    continue
+                data = protocol.get_data(data)
+                fragment_count += 1
+                file.write(data)
+            else:
+                break
+    print('\nReceived fragments are', fragment_count, 'and counted fragments are', fragment_no, '\n')
+
+
+def initialization(server_socket):
+    """ Receive initialization. Returns received data, client address, fragment size entered by user,
+    count of fragments and file name for receiveing file data tranfer.
+
+     server_socket: Server socket contains source address and sendto method. """
+
+    while True:
+        ready = select.select([server_socket], [], [], 20)
+        if ready[0]:
+            data, client_address = server_socket.recvfrom(protocol.DEFAULT_BUFF)
+        else:
+            return None
+        reply_crc = protocol.check_crc(data)
+        reply = protocol.add_header(reply_crc, 0, b'')
+        server_socket.sendto(reply, client_address)
+        if reply_crc.value == protocol.MsgType.ACK.value:
+            fragment_size = protocol.get_fragmet_size(data) + len(data)
+
+            if data.decode('utf-8')[:1] == protocol.MsgType.SET_MSG.value:
+                fragment_count = protocol.get_fragment_count(data, protocol.MsgType.SET_MSG.value)
+            else:
+                fragment_count = protocol.get_fragment_count(data)
+
+            file_name = protocol.get_file_name(data, fragment_count)
+            break
+    return data, client_address, fragment_size, fragment_count, file_name
+
+
+def receive(server_socket, dir_path):
+    """ Receive data from client and decide based on protocol header if it is text or file data transfer. """
+
+    print('Server is ready!')
+    try:
+        data, client_address, fragment_size, fragment_count, file_name = initialization(server_socket)
+    except TypeError:
+        print('Timeout')
+        return
+
+    if protocol.get_msg_type(data).decode('utf-8') == protocol.MsgType.SET.value:
+        write_file(dir_path + file_name, server_socket, fragment_size, fragment_count)
+        server_socket.sendto(bytes(protocol.MsgType.ACK.value, 'utf-8'), client_address)
+        print('Transfer successful, file at', os.path.abspath(dir_path + file_name), '\n')
+    else:
+        write_msg(server_socket, fragment_size, fragment_count)
+
+
+def set_server():
+    """ Initial server settings. Returns server port and directory path to save received files.
+    Both server port and directory path are entered by user. """
+
+    server_port = 0
+    dir_path = ''
+    while server_port == 0 or dir_path == '':
+        try:
+            if server_port == 0:
+                server_port = int(input('Enter server port to listen: '))
+
+            if server_port < 1024 or server_port > 65535:                   # validate given port
+                print('You must enter non - privileged port!')
+                server_port = 0
+                continue
+
+            dir_path = input('Enter dir path to store files in: ')
+            if not os.path.isdir(dir_path):                                 # check if given path is valid
+                print('ERROR 01: Path', dir_path, 'does not exist!')
+                dir_path = ''
+                continue
+            if dir_path[:-1] != '/':                                        # check if dir name ends with /
+                dir_path += '/'
+
+        except ValueError:                                                  # wrong data type entered
+            print('Wrong server port!')
+            server_port = 0
+            continue
+
+    return server_port, dir_path
+
+
+def user_interface():
+    """ Server user interface. """
+
+    print('\n{:^50}'.format('server'))
+    server_port, dir_path = set_server()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('', server_port))
+
+    while True:
+        print('0 - end\n'
+              '1 - receive\n'
+              '9 - change to client')
+        user_input = input('Enter what do you want to do: ')
+        if user_input == '0':
+            print('End')
+            server_socket.close()
+            sys.exit(0)
+        elif user_input == '1':
+            receive(server_socket, dir_path)
+        elif user_input == '9':
+            client.user_interface()
+        else:
+            print('\n{:^30}'.format('Wrong input! Try again.\n'))
